7aea6e27149d6070ae023caf218d8516c543929a
Nuriddagal <creakgam@gmail.com>
2025-08-14 20:42:36 +0300

    :-)

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "TODO-CI",
+  base: "/TODO-CI/",
   test: {
     globals: true,
     environment: "jsdom",
