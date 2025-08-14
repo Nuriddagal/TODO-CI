ba66cda2a0e33e4dbc2423840fd9de4334dffda2
Nuriddagal <creakgam@gmail.com>
2025-08-14 20:20:48 +0300

    deploy added

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "TODO-CI",
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./src/setupTests.ts"
-  }
+    setupFiles: "./src/setupTests.ts",
+  },
 });
